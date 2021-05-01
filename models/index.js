b93bf32b0b0b9418f68ea3d4bc62e00d87c20ee3
mtorcellini <matt.torcellini@gmail.com>
2021-04-30 21:32:10 -0400

    add many-to-many associations, pair one-to-many associations

--- models/index.js
+++ models/index.js
@@ -7,13 +7,16 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 // Categories have many Products
 
+// These are used in pairs so that each model "knows" about the other and we can perform certain sequelize functonality on either model
 Category.hasMany(Product, {
   foreignKey: 'category_id'
 });
+Product.belongsTo(Category);
 
 // Products belongToMany Tags (through ProductTag)
 // Tags belongToMany Products (through ProductTag)
-
+Product.belongsToMany(Tag, {through: ProductTag});
+Tag.belongsToMany(Product, {through: ProductTag});
 
 module.exports = {
   Product,

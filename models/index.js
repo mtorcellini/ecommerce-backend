004b300b397c5ab984ed0073a1d2c019f0bb2b22
mtorcellini <matt.torcellini@gmail.com>
2021-04-30 20:43:58 -0400

    associate Categories and Products

--- models/index.js
+++ models/index.js
@@ -5,12 +5,15 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
 // Categories have many Products
 
-// Products belongToMany Tags (through ProductTag)
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
+// Products belongToMany Tags (through ProductTag)
 // Tags belongToMany Products (through ProductTag)
+
 
 module.exports = {
   Product,

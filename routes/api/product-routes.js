4b0e4d89438e2b132d171dc0013cfd3cd6224e22
mtorcellini <matt.torcellini@gmail.com>
2021-05-02 17:40:19 -0400

    clean up comments

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,7 +6,6 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', async (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
   const data = await Product.findAll({include: [Category, Tag]});
   res.json(data);
 });
@@ -14,7 +13,6 @@ router.get('/', async (req, res) => {
 // get one product
 router.get('/:id', async (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
   const data = await Product.findByPk(req.params.id, {include : [Category, Tag]});
   res.json(data);
 });
